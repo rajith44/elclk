576d1ac4e81fbbac29b9edebcdf75bac77933757
rajithm <rajithm@flexiata.lk>
2025-12-08 07:26:04 +0530

    courier-service-ex

---
+++ extension/courier_service/admin/language/en-gb/shipping/courier_service.php
@@ -0,0 +1,47 @@
+<?php
+// Heading
+$_['heading_title'] = 'Courier Service';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Settings saved successfully!';
+$_['text_edit'] = 'Configure Courier Service';
+$_['text_general_settings'] = 'General Settings';
+$_['text_rate_configuration'] = 'Rate Configuration';
+$_['text_modal_title'] = 'Edit City Rate';
+$_['text_modal_subtitle'] = 'Fine tune delivery charges for the selected city.';
+$_['text_no_cities'] = 'No cities configured for this province yet.';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+$_['text_city_saved'] = 'City rate saved successfully!';
+
+// Entry
+$_['entry_title'] = 'Method Title';
+$_['entry_default_weight'] = 'Default Weight (kg)';
+$_['entry_additional_weight'] = 'Additional Weight Step (kg)';
+$_['entry_tax_class'] = 'Tax Class';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status'] = 'Status';
+
+// Column
+$_['column_city'] = 'City';
+$_['column_city_rate'] = 'City Rate';
+$_['column_primary_weight_rate'] = 'Primary Weight Rate';
+$_['column_additional_weight_rate'] = 'Additional Weight Rate';
+$_['column_status'] = 'Status';
+$_['column_default_method'] = 'Default Method';
+$_['column_action'] = 'Action';
+
+// Button
+$_['button_save_general'] = 'Save Settings';
+$_['button_back'] = 'Back';
+$_['button_clear'] = 'Clear';
+$_['button_close'] = 'Close';
+$_['button_apply'] = 'Apply';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Courier Service!';
+$_['error_missing_province'] = 'Please choose a province.';
+$_['error_missing_fields'] = 'Please provide both province and city.';
+$_['error_invalid_rate'] = 'Please enter valid numeric values for the rate fields.';
+
---
+++ extension/courier_service/catalog/language/en-gb/shipping/courier_service.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_title'] = 'Courier Service';
+$_['text_quote_title'] = '%s - %s';
+
